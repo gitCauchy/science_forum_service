08733336815d7e6518202bf246a4d7eae1394437
Cauchy <mailcauchy@gmail.com>
2022-09-28 13:47:20 +0800

    project init

---
+++ src/main/java/com/science/ScienceForumServiceApplication.java
@@ -0,0 +1,13 @@
+package com.science;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScienceForumServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScienceForumServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/science/ScienceForumServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.science;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScienceForumServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
