b330d6bdcfd520e5c1a7a9a06cd0a125e485bc21
Cauchy <mailcauchy@gmail.com>
2022-10-06 00:21:40 +0800

    add annotation

--- src/main/java/com/science/controller/PostController.java
+++ src/main/java/com/science/controller/PostController.java
@@ -1,9 +1,13 @@
 package com.science.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @Description: Post 控制层
+ * @Author: Cauchy
+ * @CreateTime: 2022/10/6
+ */
 @RestController
 @RequestMapping("api/post")
 public class PostController {
