d01c34ac7d1ed8631e8607da5c335b7558fad67b
Cauchy <mailcauchy@gmail.com>
2022-10-06 00:30:08 +0800

    add utils and tasks

---
+++ src/main/java/com/science/system/tasks/ScheduleTasks.java
@@ -0,0 +1,15 @@
+package com.science.system.tasks;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @Description: 定时任务
+ * @Author: Cauchy
+ * @CreateTime: 2022/10/6
+ */
+@Configuration
+@EnableScheduling
+public class ScheduleTasks {
+
+}
---
+++ src/main/java/com/science/system/utils/DateUtils.java
@@ -0,0 +1,9 @@
+package com.science.system.utils;
+
+/**
+ * @Description: 时间工具类
+ * @Author: Cauchy
+ * @CreateTime: 2022/10/6
+ */
+public class DateUtils {
+}
