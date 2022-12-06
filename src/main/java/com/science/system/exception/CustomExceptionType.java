92f80d098476287a072505a890686fba658398e9
Cauchy <mailcauchy@gmail.com>
2022-12-06 15:15:39 +0800

    custom exception

---
+++ src/main/java/com/science/system/exception/CustomExceptionType.java
@@ -0,0 +1,29 @@
+package com.science.system.exception;
+
+public enum CustomExceptionType {
+    USER_INPUT_ERROR(400, "用户输入异常"),
+    SYSTEM_ERROR(500, "系统服务异常"),
+    OTHER_ERROR(999, "未知异常");
+
+    CustomExceptionType(int code, String typeDesc) {
+        this.code = code;
+        this.typeDesc = typeDesc;
+    }
+
+    /**
+     * 异常代码
+     */
+    private int code;
+    /**
+     * 异常说明
+     */
+    private String typeDesc;
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getTypeDesc() {
+        return typeDesc;
+    }
+}
