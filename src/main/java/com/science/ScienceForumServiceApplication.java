f4d4782ebbf7e05fbacf5a4a457036388499e6fd
Cauchy <mailcauchy@gmail.com>
2022-12-06 13:58:01 +0800

    delete unused packages

--- src/main/java/com/science/ScienceForumServiceApplication.java
+++ src/main/java/com/science/ScienceForumServiceApplication.java
@@ -2,8 +2,6 @@ package com.science;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class ScienceForumServiceApplication {
